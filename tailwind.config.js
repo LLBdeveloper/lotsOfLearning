cd610ee0356abd33be81874fe24171538a358722
Lautaro Bertera <bertera.lautaro@gmail.com>
2024-03-06 01:04:43 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import plugin from "tw-elements-react/dist/plugin.cjs";
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/tw-elements-react/dist/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [plugin],
+};
