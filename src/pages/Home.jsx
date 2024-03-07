580ba4bfba1eceff94895d0685412251cc98efb5
Lautaro Bertera <bertera.lautaro@gmail.com>
2024-03-07 13:04:18 -0300

    contact ok2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="bg-[#FFF89A] flex flex-col justify-center items-center m-10 rounded-xl" >
       <h1 className=" xs:text-lg sm:text-2xl lg:text-4xl md:w-[70%] my-20 font-extrabold text-center ">Explore the World of Learning with Our English Teaching Course for <span className="text-[#FF8AAE] ">Garden Teachers!</span></h1>
-      <img src="../../public/home.png" className="max-h-[35rem] my-10 rounded-xl
+      <img src="https://firebasestorage.googleapis.com/v0/b/lotsoflearning-54445.appspot.com/o/home.png?alt=media&token=6134a9d1-b092-4110-a577-dad1b6795981" className="max-h-[35rem] my-10 rounded-xl
       " alt="" />
 
       
