1b35c9d3336303ec94c833eaa24931976c9fa24b
Lautaro Bertera <bertera.lautaro@gmail.com>
2024-03-10 00:45:34 -0300

    form ready ok. emailJS ok4

--- src/components/AlertEmail.jsx
+++ src/components/AlertEmail.jsx
@@ -9,10 +9,14 @@ function AlertEmail() {
                 open={true}
                 color="bg-success-400 text-success-800"
             >
+                <div className="xs:flex flex-col justify-center items-center sm:block xs:text-center sm:text-start">
+            
                 <strong>SENT SUCCESSFULLY !</strong>
                 <span className="ml-1">
-                Thank you for your interest, your query was sent successfully. During the next few hours you will be contacted via email or the method detailed in the query.
+                Thank you for your interest, your query was sent successfully. 
                 </span>
+                <span> During the next few hours you will be contacted via email or the method detailed in the query.</span>
+                </div>
             </TEAlert>
         </div>
     );
