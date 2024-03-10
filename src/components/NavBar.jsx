6a2d94c16ddd96316eed1d9d0cf09a88ca303e40
Lautaro Bertera <bertera.lautaro@gmail.com>
2024-03-10 00:52:00 -0300

    error navbar ok

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
       <ul
         className={
           nav
-            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
+            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500 z-50'
             : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
         }
       >
